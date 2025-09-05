58d800d73e6672d205ceec0441f90b2a7a2aa097
Ahmed Mohamed <51951611+AhmedMohamed17@users.noreply.github.com>
2025-09-05 16:30:18 +0300

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://ahmedmohamed10sayed:vuLdjXETaYeCb9Tt@dodobase.terueve.mongodb.net/?retryWrites=true&w=majority&appName=DODOBase");
   } catch (err) {
     console.log(err);
   }
