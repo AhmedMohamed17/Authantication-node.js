32652b77a7d34833ca1c0c05fe4acd67c15ddb89
Ahmed Mohamed <51951611+AhmedMohamed17@users.noreply.github.com>
2025-09-05 17:06:09 +0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   "http://localhost:5000",
   "https://authantication-node-js.vercel.app",
+  "https://authantication-node-lb8bse50a-ahmedmohamed17s-projects.vercel.app/",
+  
 ];
 
 module.exports = allowedOrigins;
